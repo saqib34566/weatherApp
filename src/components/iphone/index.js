e246e336e722aedbdf813e3bf9dfc959d74d35b6
JoshuaSeabrook <joshua.d.seabrook@gmail.com>
2023-03-15 16:24:06 +0000

    Leon Ver. Added Location, Dynamic Background.

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -15,16 +15,35 @@ export default class Iphone extends Component {
 	constructor(props){
 		super(props);
 		// temperature state
-		this.state.temp = "";
+		this.state.tempf = "";
+		this.state.tempc = "";
+		this.state.locate = "";
+		this.state.cond = "";
+		this.state.bgClass = "";
 		// button display state
-		// this.setState({ display: true });
+		this.setState({ display: true });
 		this.fetchWeatherData()
 	}
 
+	componentDidMount(){
+		// Get User Location
+		if (navigator.geolocation){
+			navigator.geolocation.getCurrentPosition(
+				position => {
+					const {latitude, longitude} = position.coords;
+					this.fetchWeatherData(latitude, longitude); 
+				},
+				error => console.log(error)
+			);
+		} else {
+			console.log('Geolocation is not supported by this browser.');
+		}
+	}
 	// a call to fetch weather data via wunderground
-	fetchWeatherData = () => {
-		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
-		var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=a56c74c2c1de022a6585f8d9b816f8fa";
+	fetchWeatherData = (latitude, longitude) => {
+		const apiKey = "a11a125eccd08918de831c6cd39c6d9e"
+		// API URL with a structure of : http://api.wunderground.com/api/key/feature/q/country-code/city.json
+		var url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
@@ -42,31 +61,43 @@ export default class Iphone extends Component {
 		
 		// display all weather data
 		return (
-			<div class={ style.container }>
+				<div class={style[`${this.state.bgClass}`]}>
 				<div class={ style.header }>
 					<div class={ style.city }>{ this.state.locate }</div>
 					<div class={ style.conditions }>{ this.state.cond }</div>
-					<span class={ tempStyles }>{ this.state.temp }</span>
+					<span class={ tempStyles }>{ this.state.tempc }</span>
+					<span class={ tempStyles }>{ this.state.tempf }</span>
 				</div>
 				<div class={ style.details }></div>
-				<div class= { style_iphone.container }> 
-					{ this.state.display ? <Button class={ style_iphone.button } clickFunction={ this.fetchWeatherData }/ > : null }
-			
-				</div>
 			</div>
 		);
 	}
 
 	parseResponse = (parsed_json) => {
 		var location = parsed_json['name'];
-		var temp_c = parsed_json['main']['temp'];
-		var conditions = parsed_json['weather']['0']['description'];
+    	var temp_c = parsed_json['main']['temp'] - 273.15;
+    	var temp_f = (temp_c * 1.8) + 32;
+    	var conditions = parsed_json['weather']['0']['description'];
+		var bgClass;
 
+		if (conditions.includes('clear')) {
+			bgClass = 'background-sunny';
+		} else if (conditions.includes('cloud')) {
+			bgClass = 'background-cloudy';
+		} else if (conditions.includes('rain')) {
+			bgClass = 'background-rainy';
+		} else if (conditions.includes('snow')) {
+			bgClass = 'background-snowy';
+		} else {
+			bgClass = '';
+		}
 		// set states for fields so they could be rendered later on
 		this.setState({
 			locate: location,
-			temp: temp_c,
-			cond : conditions
+			tempc: `${temp_c.toFixed(1)}°C`,
+			tempf: `${temp_f.toFixed(1)}°F`,
+			cond: conditions,
+			bgClass: bgClass
 		});      
-	}
+	  }
 }
