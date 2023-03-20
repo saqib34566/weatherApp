8c0c4184e287bd676d22abeb75c974cefa5567c2
saqib34566 <saqibn34566@gmail.com>
2023-03-20 01:34:00 +0000

    hourly Forcast added and working

--- src/components/forecast/index.js
+++ src/components/forecast/index.js
@@ -6,10 +6,34 @@ import style from './style';
 // make a new class and export it
 export default class Forecast extends Component {
     render() {
-
+        // const{hourlyForecast} = this.props;
+        // console.log(hourlyForecast)
+        const{hourlyForecast} = this.props;
         return (
             <div class={style.container}>
-                <h1>Forecast</h1>
+                <div class={style.header}>
+                    <p class={style.headerTitle}>hourly forcast</p>
+                </div>
+
+                <hr class={style.hr}></hr>
+
+                <div class={style.weatherRow}>
+
+                    {hourlyForecast.map((hour, index) => (
+                        <div class={style.weatherHour}>
+                        <p class={style.p} >{new Date(hour.dt_txt).toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', hour12: true})}</p>
+                        <img src={`https://openweathermap.org/img/wn/${hour.weather[0].icon}.png`} class={style.image}></img>
+                        <p>{Math.round(hour.main.temp)}°</p>
+                        </div>
+                    ))}
+
+
+                    {/* <div class={style.weatherHour}>
+                        <p>04:30 PM</p>
+                        <img src='https://openweathermap.org/img/wn/02d@2x.png' class={style.image}></img>
+                        <p>22°</p>
+                    </div> */}
+                </div>
             </div>
         );
     }
--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -23,6 +23,7 @@ export default class Iphone extends Component {
 		this.state.cond = "";
 		this.state.bgClass = "";
 		this.state.currentIcon = "";
+		this.state.hourlyForecast = [];
 		// button display state
 		this.fetchWeatherData();
 		setInterval(this.fetchWeatherData(), 60000); //Refreshes weather data every 60 seconds
@@ -41,24 +42,40 @@ export default class Iphone extends Component {
 		} else {
 			console.log('Geolocation is not supported by this browser.');
 		}
+
+		fetch(`https://api.openweathermap.org/data/2.5/forecast?q=London&appid=a11a125eccd08918de831c6cd39c6d9e&units=metric`)
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+				this.setState({
+					//get 5 items from list
+					hourlyForecast: data.list.slice(0, 5)
+					//hourlyForecast: data.list 
+				});
+				console.log(this.state.hourlyForecast);
+			}
+		);
+
 	}
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = (latitude, longitude) => {
 		const apiKey = "a11a125eccd08918de831c6cd39c6d9e";
 		// API URL with a structure of : http://api.wunderground.com/api/key/feature/q/country-code/city.json
-		const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-		const openMateoUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode`;
+		//const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+		const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
+		//const openMateoUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode`;
 		$.ajax({
 			url: openWeatherUrl,
 			dataType: "jsonp",
 			success : this.openWeatherParseResponse,
 			error : function(req, err){ console.log('API call failed ' + err); }
 		});
-		$.ajax({
+		/*$.ajax({
 			url: openMateoUrl,
 			success : this.openMateoParseResponse,
 			error : function(req, err){ console.log('API call failed ' + err); }
 		});
+		*/
 		// once the data grabbed, hide the button
 		this.setState({ display: false });
 	}
@@ -79,7 +96,7 @@ export default class Iphone extends Component {
 					<img class={style.iconToday} src={this.state.currentIcon} alt="weather icon" />
 				</div>
 				<div class={ style.details }></div>
-				<Forecast/ >
+				<Forecast hourlyForecast={this.state.hourlyForecast} / >
 			</div>
 		);
 	}
@@ -114,6 +131,8 @@ export default class Iphone extends Component {
 		});      
 	}
 	  
+	openWeatherHourlyParseResponse = (parsed_json) => {}
+
 	openMateoParseResponse = (parsed_json) => {
 		var forecastTemps = [];// <---------- THE ARRAY I WAS TALKING ABOUT
 
