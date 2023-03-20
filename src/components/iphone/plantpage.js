f001e4789babe84502a60c89e92a865ace8c4f24
LeonSangster <leon0sangster@gmail.com>
2023-03-20 21:09:32 +0000

    added plantpage.js, needs implementing.

---
+++ src/components/iphone/plantpage.js
@@ -0,0 +1,66 @@
+import { h, Component, render } from 'preact';
+import fetchJsonp from 'fetch-jsonp';
+
+class PlantSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      common_name: "",
+      watering: "",
+      sunlight: "",
+      cycle:"",
+      loading: false
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({ query: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ loading: true });
+    const API_KEY = 'sk-NPVh64188e7a9a6cd260';
+    const API_URL = `https://perenual.com/api/species-list?page=1&key=${API_KEY}&q=${encodeURIComponent(this.state.query)}`;
+    fetchJsonp(API_URL)
+      .then(response => response.json())
+      .then(data => {
+        const plant = data.data[0];
+        const PLANT_INFO_URL = `https://perenual.com/api/species-list?key=${API_KEY}&q=${plant.id}`;
+        return fetchJsonp(PLANT_INFO_URL).then(response => response.json());
+      })
+      .then(data => {
+        this.setState({ common_name: data.data, loading: false, watering: data.data, sunlight: data.data, cycle: data.data });
+      })
+      .catch(error => {
+        console.error(error);
+        this.setState({ loading: false });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.query} onChange={this.handleInputChange} />
+          <button type="submit">Search</button>
+        </form>
+        {this.state.loading && <div>Loading...</div>}
+        {this.state.plantInfo && (
+          <div>
+            <h2>{this.state.plantInfo.common_name}</h2>
+            <p>Scientific name: {this.state.plantInfo.scientific_name}</p>
+            <p>Family: {this.state.plantInfo.family}</p>
+            <p>Genus: {this.state.plantInfo.genus}</p>
+            <p>Native status: {this.state.plantInfo.native_status}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+render(<PlantSearch />, document.body);
